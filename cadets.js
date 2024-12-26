2fa60ba1ce7b5a6e498441911bc07259849183ee
carparklocator <carparklocator@gmail.com>
2024-12-26 14:17:58 +0800

    Update cadets.js

--- cadets.js
+++ cadets.js
@@ -89,6 +89,7 @@ function searchCadets() {
     const searchTerm = document.getElementById('searchInput').value.toLowerCase();
     const filteredCadets = cadetsData.filter(cadet => 
         cadet.name.toLowerCase().includes(searchTerm) ||
+        cadet.swappedName.toLowerCase().Includes(searchTerm)||
         cadet.unit.toLowerCase().includes(searchTerm) ||
         cadet.contingent.toString().includes(searchTerm)
     );
